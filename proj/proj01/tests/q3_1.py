ff140411befa2e0c2af98efc71489d44ce3afaed
Rohan Jha <rohan.jha@berkeley.edu>
2022-08-26 17:37:57 -0700

    added proj01

---
+++ proj/proj01/tests/q3_1.py
@@ -0,0 +1,12 @@
+test = {   'name': 'q3_1',
+    'points': None,
+    'suites': [   {   'cases': [   {   'code': '>>> assert type(total_demand) == int\n'
+                                               '>>> assert type(total_supply) == int\n'
+                                               '>>> assert total_demand > 0 and total_demand < 10000\n'
+                                               '>>> assert total_supply > 10000 and total_supply < 20000\n',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
