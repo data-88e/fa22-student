3cd3b56f0f1ff1206355787c2aa111d396530d78
Rohan Jha <rohan.jha@berkeley.edu>
2022-09-23 12:33:53 -0700

    added lab04 for test

---
+++ lab/lab04/tests/q1_3.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q1_3',
+    'points': [0],
+    'suites': [{'cases': [{'code': '>>> q1_3 in ["a", "b", "c", "d"]\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
