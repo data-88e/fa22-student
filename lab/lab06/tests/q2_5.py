ec51e99737dc58f1be9313bce8b1a6eaeecd357e
Yiyang Chen <yiyangc@berkeley.edu>
2022-10-09 18:18:25 -0700

    Added lab06

---
+++ lab/lab06/tests/q2_5.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q2_5',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> us_log.num_rows == 6\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> us_log.num_columns == 2\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> sum(us_log.column("log_income_percentile")) < 0\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
