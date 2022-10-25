6b4c5fc2467357345f34c47ced2df3a039df15ee
Bennett Somerville <13303021+bdsomer@users.noreply.github.com>
2022-10-24 23:39:45 -0700

    add lab08

---
+++ lab/lab08/tests/q2_5.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2_5',
+    'points': [0, 0],
+    'suites': [   {   'cases': [{'code': '>>> assert 35 < float(p_m) < 50\n', 'hidden': False, 'locked': False}, {'code': '>>> assert 10 < float(q_m) < 20\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
